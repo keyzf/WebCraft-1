663394fb591ee997c7e60be8100b437ca5753764
somePythonProgrammer <leenagajbhiye1003@gmail.com>
2021-06-29 18:49:55 +0530

    using simplex noise

--- sketch.js
+++ sketch.js
@@ -5,31 +5,6 @@ var player,
   text,
   simplex = new SimplexNoise('12047073465');
 
-/*window.onbeforeunload = function (e) {
-    // Cancel the event
-    e.preventDefault();
-
-    // Chrome requires returnValue to be set
-    e.returnValue = 'Really want to quit the game?';
-};
-
-//Prevent Ctrl+S (and Ctrl+W for old browsers and Edge)
-document.onkeydown = function (e) {
-    e = e || window.event;//Get event
-
-    if (!e.ctrlKey) return;
-
-    var code = e.which || e.keyCode;//Get key code
-
-    switch (code) {
-        case 83:
-        case 87:
-            e.preventDefault();
-            e.stopPropagation();
-            break;
-    }
-};*/
-
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   console.log("loading...");

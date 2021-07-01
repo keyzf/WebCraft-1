64365bf5cbe6ec04c29039caeaff5c3644f0e517
somePythonProgrammer <leenagajbhiye1003@gmail.com>
2021-07-01 09:46:07 +0530

    added a little walking mechanism

--- js/Block.js
+++ js/Block.js
@@ -1,5 +1,5 @@
 class Block {
-  constructor(x, z,parent) {
+  constructor(x, z, parent) {
     this.pos = { x: x, z: z };
     //console.log(this.pos)
     this.dimensions = createVector(10, 10, 10);
@@ -8,16 +8,16 @@ class Block {
     this.h =
       round(
         simplex.noise2D(i / 20, j / 20) * 1 +
-        simplex.noise2D(i / 350, j / 350) * 2 +
-        simplex.noise2D(i / 550, j / 550) * 5 +
-        simplex.noise2D(i / 800, j / 800) * 10 +
-        simplex.noise2D(i / 2000, j / 2000) * 100 +
-        simplex.noise2D(i / 10000, j / 10000) * 500
+          simplex.noise2D(i / 350, j / 350) * 2 +
+          simplex.noise2D(i / 550, j / 550) * 5 +
+          simplex.noise2D(i / 800, j / 800) * 10 +
+          simplex.noise2D(i / 2000, j / 2000) * 100 +
+          simplex.noise2D(i / 10000, j / 10000) * 500
       ) * 10;
     this.position = createVector(x / 10, this.h, z / 10);
     this.shown = true;
-    this.current = false
-    this.parent = parent
+    this.current = false;
+    this.parent = parent;
   }
   render() {
     if (this.shown) {
@@ -30,7 +30,7 @@ class Block {
   }
   update() {
     //this.showhide();
-    this.playerInteract()
+    this.playerInteract();
   }
   showhide() {
     if (
@@ -53,13 +53,19 @@ class Block {
     }
   }
   playerInteract() {
-    if(round(player.position.x / 10)===this.pos.x&&round(player.position.z / 10)===this.pos.z){
-      this.current= true
-      world.currPlayerChunk = {x:this.parent.position.x,z:this.parent.position.z}
+    if (
+      round(player.position.x / 10) === this.pos.x &&
+      round(player.position.z / 10) === this.pos.z
+    ) {
+      this.current = true;
+      player.position.y = this.h-40
+      world.currPlayerChunk = {
+        x: this.parent.position.x,
+        z: this.parent.position.z,
+      };
       //console.log(world.midChunk)
-    }
-    else{
-      this.current = false
+    } else {
+      this.current = false;
     }
   }
 }
--- test.js
+++ test.js
@@ -1,90 +1,51 @@
 //backup file,will be removed soon.
 
-var cubes = [];
-var player,
-  playerTouching = false,
-  chunkWidth=60,
-  chunkDepth=60;
-fallSpeed = 20
-
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  for (i = 0; i < chunkWidth; i++) {
-    cubes[i] = [];
-    for (j = 0; j < chunkDepth; j++) {
-      cubes[i].push(new block(i, j));
-    }
-  }
-  player = createRoverCam();
-  player.usePointerLock();
-  player.setState({ speed: 5 });
-  player.position.y = cubes[round(chunkWidth/2)][round(chunkDepth/2)].h-100
-  player.position.x = cubes[round(chunkWidth/2)][round(chunkDepth/2)].pos.x
-  player.position.z = cubes[round(chunkWidth/2)][round(chunkDepth/2)].pos.z
+  createCanvas(500, 500, WEBGL);
 }
 
 function draw() {
+  angleMode(DEGREES);
+  orbitControl();
   background("black");
-  normalMaterial();
-  if (!playerTouching) {
-    player.position.y += fallSpeed;
-  } else if (playerTouching) {
-    if (getPlayerTouchingGround()) {
-      playerTouching = true;
-      try {
-        player.position.y =
-          cubes[round(player.position.x / 10)][round(player.position.z / 10)]
-            .h - 50;
-      } catch (error) {
-        playerTouching = false;
-      }
-    }
-    else if(keyIsDown(32)){
-      player.position.y -= 10
-      playerTouching = false;
-    }
-    else{
-      try {
-        player.position.y =
-          cubes[round(player.position.x / 10)][round(player.position.z / 10)]
-            .h - 50;
-      } catch (error) {
-        playerTouching = false;
-      }
-    }
-  }
-  if (getPlayerTouchingGround()) {
-    playerTouching = true;
-    fallSpeed = 10
-    try {
-      player.position.y =
-        cubes[round(player.position.x / 10)][round(player.position.z / 10)].h -
-        50;
-    } catch (error) {
-      playerTouching = false;
-    }
-  }
-  for (var i in cubes) {
-    for (var j in cubes[i]) {
-      cubes[i][j].render();
-      cubes[i][j].playerInteract();
-      cubes[i][j].update();
-    }
-  }
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+  //front
+  push();
+  translate(0, 0, 5);
+  plane(10, 10);
+  pop();
 
-function getPlayerTouchingGround() {
-  try {
-    return (
-      cubes[round(player.position.x / 10)][round(player.position.z / 10)].h -
-        20 <
-      player.position.y + 20
-    );
-  } catch (error) {
-    return false;
-  }
+  //back
+  push();
+  translate(0, 0, -5);
+  plane(10, 10);
+  pop();
+
+  //right
+  push();
+  translate(5, 0, 0);
+  rotateY(90);
+  plane(10, 10);
+  pop();
+
+  //left
+  push();
+  translate(-5, 0, 0);
+  rotateY(90);
+  plane(10, 10);
+  pop();
+
+  //top
+  push();
+  translate(0, 5, 0);
+  rotateX(90);
+  plane(10, 10);
+  pop();
+
+  //bottom
+  push();
+  translate(0, -5, 0);
+  rotateX(90);
+  plane(10, 10);
+  pop();
 }
